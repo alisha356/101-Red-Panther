e38d833f8e1ec4967b458fbd30fb0a768e9f6db6
Alisha <21amessingham@wakatipu.school.nz>
2026-05-04 21:36:58 +1200

    Copy starting.c as base

---
+++ main.c
@@ -0,0 +1,52 @@
+/*
+ * The code below is meant to detect whether the Red Ruby
+ * is present. The starategy implemented by the code is not very effective. 
+ * Study the code so that you understand what the strategy is and how 
+ * it is implemented. Then design and implement a better startegy.
+ * */
+
+
+
+#include <stdio.h>
+#include "camera_x11.h"
+
+//using namespace std;
+
+int main() {
+	
+	 if (start_camera() != 0) {
+        printf("Failed to start camera\n");
+        return 1;
+    }
+  //int err = init(0);
+  //cout<<"Error: "<<err<<endl;
+  //open_screen_stream();
+  
+  // make 1000 runs  
+  for (int countrun = 0; countrun < 1000; countrun++) {
+	take_picture();
+	display_picture();
+	int totRed = 0;
+	int totInt = 0;
+	double redness = 0.0;
+    // for all pixels in latest image
+    for (int row = 0 ; row < 480 ; row++) {	
+		for (int col = 0; col < 640; col++) {
+			uchar r, g ,b ;
+			get_pixel(row, col, &r, &g ,&b);
+			totRed = totRed + r;
+			totInt = totInt + (r+g+b)/3;
+			redness = (double)totRed/(3.0*(double)totInt);
+		}
+	}
+
+	printf(" countrun: =%d\n",countrun);
+	//cout<<" Total red: "<<totRed<<endl;
+	//cout<<", Total intensity: "<<totInt<<endl;
+	//cout<<", redness : "<<redness<<endl<<endl;
+	//sleep1(1000); // slow down a bit to make display easier
+	
+  }  
+  //close_screen_stream();
+  return 0;
+}
